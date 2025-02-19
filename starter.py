80cf6d6f540f5baa304dd3e4c22181fb796765a8
Brohudev <hitarth.thanki9006@gmail.com>
2025-02-18 18:04:17 -0600

    Add code

--- starter.py
+++ starter.py
@@ -1,1 +1,12 @@
-# todo: make this file
+#!/usr/bin/env python3
+import sys
+
+def main():
+    if not sys.stdin.isatty():
+        print("Running in piped mode... bootstrapping...")
+        # Add your bootstrap logic here
+    else:
+        print("Error: Please run this script using 'curl -sSL <URL> | python3 starter.py'")
+
+if __name__ == '__main__':
+    main()
