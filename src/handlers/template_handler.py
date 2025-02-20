b4b939e9fccb87407cdb1ecabba9cc3ab88a1482
Brohudev <hitarth.thanki9006@gmail.com>
2025-02-19 22:58:57 -0600

    structure the project much better

---
+++ src/handlers/template_handler.py
@@ -0,0 +1,7 @@
+def generic_handler(stack_name):
+    print(f"""⚠️  {stack_name.replace('_',' ').title()} setup is either:
+- Not currently released
+- Being actively developed
+- In closed beta
+
+Contribute at github.com/brohudev/spawn-point""")
--- src/spawnpoint.py
+++ src/spawnpoint.py
@@ -1,12 +1,13 @@
 import platform, os
-try:
-    if platform.system() != 'Linux':
-        raise Exception(f"{platform.system()}")
-    with open('/etc/os-release') as f:
-        os_info = {k:v.strip('"') for k,v in (line.strip().split('=') for line in f if '=' in line)}
-    if os_info.get('ID') == 'fedora' and int(os_info.get('VERSION_ID',0)) >= 41:
-        print("im working")
-    else:
-        raise Exception(f"Fedora {os_info.get('VERSION_ID','<unknown>')}")
-except Exception as e:
-    print(f"{str(e)} is not supported, please check back later or open a pr with support for your distro at github.com/brohudev/spawn-point")
+
+def verify_platform():
+     if platform.system() != 'Linux':
+          raise Exception(platform.system())
+          
+     with open('/etc/os-release') as f:
+          os_info = {k:v.strip('"') for k,v in (line.strip().split('=', 1) for line in f if '=' in line)}
+
+     if os_info.get('ID') != 'fedora' or int(os_info.get('VERSION_ID', 0)) < 41:
+          raise Exception(f"Fedora {os_info.get('VERSION_ID', '')}".strip())
+
+     return True
